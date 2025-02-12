df1d92eb18452927b3eff46751dfeeb1575a1c9c
Bajura Andrei <slayer2004@list.ru>
2025-02-13 02:15:19 +0300

    every

--- cpp-project/src/Sniffer.cpp
+++ cpp-project/src/Sniffer.cpp
@@ -1,4 +1,46 @@
 #include "Sniffer.h"
 
+snif::Sniffer::Sniffer(const SnifferParams &params) : params_{params} {
 
+  char errbuf[PCAP_ERRBUF_SIZE];
 
+  switch (params_.device_type) {
+  case InputDevice::Undefined: {
+    pcap_if_t *devs[3];
+    if (pcap_findalldevs(devs, errbuf) != 0 || devs[0] == nullptr) {
+      throw SnifferException(std::string(errbuf));
+    }
+    params_.device_arg = devs[0]->name;
+    device_ = pcap_open_live(params_.device_arg, BUFSIZ, 1, 1000, errbuf);
+
+    break;
+  }
+  case InputDevice::Interface: {
+    device_ = pcap_open_live(params_.device_arg, BUFSIZ, 1, 1000, errbuf);
+    break;
+  }
+  case InputDevice::File: {
+    device_ = pcap_open_offline(params_.device_arg, errbuf);
+    break;
+  }
+  }
+
+  if (!device_) {
+    throw SnifferException(std::string(errbuf));
+  }
+}
+
+snif::Sniffer::~Sniffer() {
+  if (device_ != nullptr) {
+    pcap_close(device_);
+  }
+}
+
+void snif::Sniffer::process() {
+
+    pcap_compile("")
+
+}
+void snif::Sniffer::write_to_stdout() {}
+
+void snif::Sniffer::write_to_csv() 
--- cpp-project/src/main.cpp
+++ cpp-project/src/main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+#include "Sniffer.h"
+
 int main(int argc, char **argv) {
 
+  if (argc == 2 && std::strcmp("-h", argv[1]) == 0 ||
+      std::strcmp("--help", argv[1]) == 0) {
+        
+  }
 }
